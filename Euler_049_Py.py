9e480683ff79fda951fc50fef286def68415d57c
Ariel Tynan <tynan.a@northeastern.edu>
2022-03-05 15:24:39 -0500

    Answer found! Realized I needed to consider combinations/permutations with replacement. Used product function for allComb instead of combination function. Code is still inefficient, compiles in ~20s. Problem is solved but code can be improved by eliminating redundancies in the search

--- Euler_049_Py.py
+++ Euler_049_Py.py
@@ -27,37 +27,35 @@ def prime_Sieve(n): #Function modified from Geeksforgeeks
 
 primes = prime_Sieve(10000) #all 4 digit primes
 
-#Using digits 1,2,3,4,5,6,7,8,9 (assuming no 0 even though it is possible, but unlikely)
+#Using digits 1,2,3,4,5,6,7,8,9 
 #Generate permutations, 9 choose 4
 import itertools
 from itertools import permutations
 from itertools import combinations
-allComb = combinations([1,2,3,4,5,6,7,8,9],4)
-
-
-temp1 = temp2 = 0 #temp variables for averaging permutations
-success = False
-trials = 0 #9 choose 4 combinations limits to 126 trials
-
+allComb = itertools.product([1,2,3,4,5,6,7,8,9],repeat = 4) #should update to allow replacement
+validList = []
 for i in list(allComb):
-    permTemp = permutations(i,4)
+    permTemp = permutations(i,4) #take permutation of each set of combinations
     perm = [] #temporary permutation list, used for iterating
     for q in list(permTemp): # for each of the 24 permutations, push back into usable list
         perm.append(q)
     for j in range(len(perm)): #24 permutations in each set (4!)
         for k in range(j+1,len(perm)):
-            #print(i, perm[j])
-            if perm[j][3] % 2 == 1 and perm[k][3] % 2 == 1: #Both must be odd
+            if perm[j][3] % 2 == 1 and perm[k][3] % 2 == 1 and perm[j] != perm[k]: #Both must be odd
                 temp1 = perm[j][0]*1000 + perm[j][1]*100 + perm[j][2]*10 + perm[j][3] #first num
                 temp2 = perm[k][0]*1000 + perm[k][1]*100 + perm[k][2]*10 + perm[k][3] #second num
-                ave = (temp1 + temp2)/2
+                ave = int((temp1 + temp2)/2) #in arithmetic sequence, ave of first and third term equals second term
                 if temp1 in primes and temp2 in primes and ave in primes:
                     for x in perm:
                         temp3 = x[0]*1000 + x[1]*100 + x[2]*10 + x[3]
-                        if temp3 == ave:
-                                print("SUCCESS")
-                                print(temp1,ave,temp2)
-                                #success = True
+                        if temp3 == ave and temp1 < ave < temp2:
+                                #print("SUCCESS")
+                                answer = perm[j] + x + perm[k]
+                                if answer not in validList:
+                                    print(answer)
+                                    validList.append(answer)
+                                    #success = True
+                                
                     
 
 
